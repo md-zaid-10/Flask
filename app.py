22e040e71525db9b5918d675f06ff124a2045c45
md-zaid-10 <mdzaid.de.123@gmail.com>
2026-07-23 19:45:42 +0200

    Flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+'''
+It creates an instance of flask class, which will be your WSGI (web server gateway interface) application.
+'''
+
+# initialize flask WSGI app
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to this App. This is an amazing Framework"
+
+@app.route("/index")
+def index():
+    return "Welcome to the index page"
+
+# this is an entry point for your application
+if __name__=="__main__":
+    app.run(debug=True)
